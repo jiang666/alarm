61c26eafde66d6ad0a1cd7b6299ef17fa96065c6
jll <jll@pro-shine.cn>
2022-01-11 19:25:32 +0800

    添加日历

--- app/src/main/java/com/example/alarm/viewpager/DataqueryFragment.java
+++ app/src/main/java/com/example/alarm/viewpager/DataqueryFragment.java
@@ -2,47 +2,39 @@ package com.example.alarm.viewpager;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 import com.example.alarm.R;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
+import com.example.alarm.adapter.DateMonthAdapter;
+import com.example.alarm.utils.DataUtils;
 
 /**
  * 数据查询
  */
-public class DataqueryFragment extends Fragment {
+public class DataqueryFragment extends Fragment implements View.OnClickListener{
     private static final String TAG = DataqueryFragment.class.getSimpleName();
-    @BindView(R.id.id_timetablecontent)
-    FrameLayout idTimetablecontent;
-    @BindView(R.id.tv_remotedata)
-    TextView tvRemotedata;
-    @BindView(R.id.tv_telemetrydata)
-    TextView tvTelemetrydata;
-    @BindView(R.id.tv_telemetryrealline)
-    TextView tvTelemetryrealline;
-    @BindView(R.id.tv_telemetryhistory)
-    TextView tvTelemetryhistory;
-    @BindView(R.id.ll_switch)
-    LinearLayout llSwitch;
-    @BindView(R.id.tv_pagetype)
-    TextView tvPagetype;
-    private View mRootView;
-    private Unbinder unbinder;
-    private static final int REMOTE_FRAGMENT = 0;
-    private static final int TELEMETRY_FRAGMENT = 1;
-    private static final int TELEMETRYREAL_FRAGMENT = 2;
-    private static final int TELEMETRYHISTORY_FRAGMENT = 3;
 
+    private View mRootView;
+    private GridView gridView;
+    private TextView frontMonthTv,nextMonthTv,ok;
+    private GestureDetector gestureDetector;
+    private TextView currentDateTv;
+    private String date;
+    private DateMonthAdapter adapter;
+    final int RIGHT = 0;
+    final int LEFT = 1;
 
     public DataqueryFragment() {
         // Required empty public constructor
@@ -70,23 +62,123 @@ public class DataqueryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mRootView = inflater.inflate(R.layout.remote_data_layout, container, false);
-        unbinder = ButterKnife.bind(this, mRootView);
+        mRootView = inflater.inflate(R.layout.pop_layout, container, false);
+        gridView = (GridView) mRootView.findViewById(R.id.list);
+        frontMonthTv = (TextView)mRootView.findViewById(R.id.front_month);
+        frontMonthTv.setOnClickListener(this);
+        nextMonthTv = (TextView)mRootView.findViewById(R.id.next_month);
+        nextMonthTv.setOnClickListener(this);
+        ok = (TextView)mRootView.findViewById(R.id.ok);
+        ok.setOnClickListener(this);
+        gestureDetector = new GestureDetector(getContext(),onGestureListener);
+        currentDateTv = (TextView)mRootView.findViewById(R.id.now_month);
+        this.date = "2022-01-11" ;
+        if (TextUtils.isEmpty(date)){
+            this.date = DataUtils.getCurrDate("yyyy-MM-dd");
+        }
+        currentDateTv.setText("当前月份："+ DataUtils.formatDate(date,"yyyy-MM"));
+        adapter = new DateMonthAdapter(getContext());
+        adapter.setData(DataUtils.getMonth(date));
+        gridView.setAdapter(adapter);
+        adapter.setDateString(date);
+        adapter.setSelectedPosition(DataUtils.getSelectPosition());
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //if (onItemClick!=null && !TextUtils.isEmpty(adapter.getItem(position).date)){
+                if (!TextUtils.isEmpty(adapter.getItem(position).date)){
+                    adapter.setSelectedPosition(position);
+                    currentDateTv.setText("当前月份："+DataUtils.formatDate(adapter.getItem(position).date,"yyyy-MM"));
+                    date = adapter.getItem(position).date ;
+                }
+            }
+        });
+        gridView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        });
+
         return mRootView;
+    }
+    /**
+     * 手势监听是否是左右滑动
+     */
+    private GestureDetector.OnGestureListener onGestureListener =
+            new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                                       float velocityY) {
+                    float x = e2.getX() - e1.getX();
+                    float y = e2.getY() - e1.getY();
+
+                    if (x > 100) {
+                        doResult(RIGHT);
+                    } else if (x < -100) {
+                        doResult(LEFT);
+                    }
+                    return true;
+                }
+            };
+
+    public void doResult(int action) {
+
+        switch (action) {
+            case RIGHT:
+                date = DataUtils.getSomeMonthDay(date,-1);
+                adapter.setData(DataUtils.getMonth(date));
+                adapter.setDateString(date);
+                adapter.setSelectedPosition(DataUtils.getSelectPosition());
+                currentDateTv.setText("当前月份："+DataUtils.formatDate(date,"yyyy-MM"));
+                Log.e("wenzihao","go right");
+                break;
+            case LEFT:
+                date = DataUtils.getSomeMonthDay(date,+1);
+                adapter.setData(DataUtils.getMonth(date));
+                adapter.setDateString(date);
+                adapter.setSelectedPosition(DataUtils.getSelectPosition());
+                currentDateTv.setText("当前月份："+DataUtils.formatDate(date,"yyyy-MM"));
+                Log.e("wenzihao","go left");
+                break;
+
+        }
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id==frontMonthTv.getId()){
+            date = DataUtils.getSomeMonthDay(date,-1);
+            adapter.setData(DataUtils.getMonth(date));
+            adapter.setDateString(date);
+            adapter.setSelectedPosition(DataUtils.getSelectPosition());
+            currentDateTv.setText("当前月份："+DataUtils.formatDate(date,"yyyy-MM"));
+        }else if (id==nextMonthTv.getId()){
+            date = DataUtils.getSomeMonthDay(date,+1);
+            adapter.setData(DataUtils.getMonth(date));
+            adapter.setDateString(date);
+            adapter.setSelectedPosition(DataUtils.getSelectPosition());
+            currentDateTv.setText("当前月份："+DataUtils.formatDate(date,"yyyy-MM"));
+        }else if (id==ok.getId()){
+            if (onItemClick!=null){
+                onItemClick.onItemClick(date);
+            }
+        }
     }
+    /**
+     * 点击回调接口
+     */
+    public interface OnItemClick{
+        void onItemClick(String date);
+    }
+    public OnItemClick onItemClick ;
 
-
+    public void setOnItemClick(OnItemClick onItemClick) {
+        this.onItemClick = onItemClick;
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        if (unbinder != null) {
-            unbinder.unbind();
-        }
     }
 }
